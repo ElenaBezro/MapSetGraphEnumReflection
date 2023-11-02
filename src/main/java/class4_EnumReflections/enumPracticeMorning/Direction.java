2895c3bf685c1873cc6025325e4b23773c9eaefb
Elena Bezrodnova <lenyka_86@mail.ru>
2023-11-02 23:19:12 +0100

    remove comment

--- src/main/java/class4_EnumReflections/enumPracticeMorning/Direction.java
+++ src/main/java/class4_EnumReflections/enumPracticeMorning/Direction.java
@@ -1,17 +1,6 @@
 package class4_EnumReflections.enumPracticeMorning;
 
 public enum Direction {
-    //
-
-//TODO:
-
-    //4.Directional Constants:
-    //Create an enum named Direction to represent cardinal directions (NORTH, SOUTH, EAST, WEST).
-    //Define constants for each direction within the enum.
-    //Implement a constructor in the enum that takes two integers, deltaX and deltaY, representing changes in x and y coordinates when moving in that direction.
-    //Implement methods in the enum to calculate new x and y coordinates based on the current position and direction.
-    //Demonstrate the usage of the Direction enum by simulating movement in different directions from a given starting position.
-    //
     NORTH(0, 5),
     SOUTH(0, 3),
     EAST(10, 0),
